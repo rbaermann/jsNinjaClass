7e5df22a1a1716a14c38e5f782a846d527ca4c59
Ryan Baermann <rcbaermann@gmail.com>
2019-09-05 10:12:10 -0700

    added punch/kick. also did bug fixes

--- ninja.js
+++ ninja.js
@@ -4,15 +4,39 @@ function Ninja(name) {
     var speed = 3;
     var strength = 3;
 
-    Ninja.prototype.sayName() = () => {
+    this.sayName = () => {
         console.log(`Hi; my name is ${name}!`);
     }
 
-    Ninja.prototype.showStats() = () => {
-        console.log(`Name: ${name}, Health: ${health}, Speed: ${speed}, Strength: ${strength}.`);
+    this.showStats = () => {
+        console.log(`Name: ${this.name}, Health: ${this.health}, Speed: ${speed}, Strength: ${strength}.`);
     }
 
-    Ninja.drinkSake() = () => {
+    this.drinkSake = () => {
         this.health += 10;
+        console.log(`${this.name}'s Health raised by 10!`)
+    }
+
+    this.punch = (target) => {
+        if(!(target instanceof Ninja)) {
+            console.log(`${target} is not a Ninja!`)
+        }
+        else {
+            target.health -= 5;
+            console.log(`${target.name} was punched by ${this.name} and lost 5 Health!`)
+        }
+    }
+
+    this.kick = (target) => {
+        if(!(target instanceof Ninja)) {
+            console.log(`${target} is not a Ninja!`)
+        }
+        else {
+            target.health -= 15 * this.strength;
+            console.log(`${target.name} was kicked by ${this.name} and lost 15 Health!`)
+        }
     }
 }
+
+var blueNinja = new Ninja("Goemon");
+var redNinja = new Ninja("Bill Gates");
